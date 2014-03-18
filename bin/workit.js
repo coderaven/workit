74d53543577a96b6b675c43698b6e264c336999b
Raven G. Duran <me@imraven.com>
2014-03-18 13:56:21 +0800

    Basic Setup with Persistent Database

--- bin/workit.js
+++ bin/workit.js
@@ -1,3 +1,35 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+//console.log(process.cwd()); // List directory currently in
+var path = require('path');
+var program = require('commander');
+var db = require('dirty')('workit.db');
+
+var projects = {};
+
+db.on('load', function() {
+  	if (db.get('projects') === undefined){
+  		console.log("error!");
+  		projects.count = 0;
+		db.set('projects',projects);
+	} else {
+		projects = db.get('projects');
+		projects.count++;
+		db.set('projects',projects);
+		console.log(db.get('projects').count);
+	}  
+});
+
+
+program
+  .version('0.0.0')
+
+program
+  .command('add')
+  .description('add directory to a project')
+  .action(function(){
+        console.log('Hi my Friend!!!');
+});
+
+
+program.parse(process.argv);
