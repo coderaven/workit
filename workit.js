fd18a2e66f3886577579de2fd58f5a6f4f88f991
Raven G. Duran <me@imraven.com>
2014-03-21 15:01:56 +0800

    Working Add and List Functions

--- workit.js
+++ workit.js
@@ -5,53 +5,60 @@ var path = require('path');
 var program = require('commander');
 var fs = require('fs');
 
+// Basic File Data Structure
 var workit = {};
 workit.count = 0;
 workit.projects = [];
 
-var saveData = function(){
+var saveData = function(callback){
 	fs.writeFile("projects.db", JSON.stringify(workit), function (err){
 		if (err) throw err;
-	});
-}
-
-var loadData = function(){
-	fs.readFile("projects.db", function(err,data){
-    if (!err){
-      workit = data;
-      saveData();
+    else {
+      if (!(callback === undefined)){
+        callback();
+      }
     }
 	});
 }
+
+var loadData = function(callback){
+	fs.readFile("projects.db", function(err,data){
+    if (!err) {
+      workit = JSON.parse(data);
+    } 
+
+    saveData(callback);
+	});
+}
  
-loadData();
 
-
-program
+loadData(function(){
+  program
   .version('0.0.0')
 
-program
-  .command('add [ProjectName]')
-  .description('register / add a project folder to WorkIT')
-  .action(function(projectName){
-  		var loc = process.cwd() + "/" + projectName;
-        console.log('Project Name: ' + projectName);
-        console.log('Location: ' + loc);
-        workit.projects.push({name: projectName, location: loc});
-        saveData();
+  program
+    .command('add [ProjectName]')
+    .description('register / add a project folder to WorkIT')
+    .action(function(projectName){
+        var loc = process.cwd() + "/" + projectName;
+          console.log('Project Name: ' + projectName);
+          console.log('Location: ' + loc);
+          console.log("Project Successfully Added!");
+          workit.count++;
+          workit.projects.push({name: projectName, location: loc});
+          saveData();
+  });
+
+  program
+    .command('list')
+    .description('lists all projects registered in WorkIT')
+    .action(function(projectName){
+        console.log(workit.count++);
+  });
+
+  program.parse(process.argv);
 });
 
-program
-  .command('list')
-  .description('lists all projects registered in WorkIT')
-  .action(function(projectName){
-  		console.log(workit.projects.length);
-  		for (var i = 0; i < workit.projects.length; i++){
-  			console.log(i);
-  		}
-});
-
-program.parse(process.argv);
 
 
 
